9ab68d579146e4db5442d6909a8e7893207e74a8
Sloan Haywood <haywood.sloan@yahoo.com>
2020-03-31 13:00:15 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const os = require("os");
+
+// Create the express app instance
+const app = express();
+const port = 3000;
+
+// Use a static router for serving our pages
+const static = express.static(`${__dirname}/static`);
+app.use(static);
+
+// Add handlers for our APIs
+app.get("/api/status", (req, res) => {
+    res.statusCode = 200;
+    res.end(`${os.hostname()} - ${os.platform()} - ${os.release()}`);
+})
+
+// Start the Express app listening
+app.listen(port, () => {
+    console.log(`Started Express app listening on port ${port}`);
+});
