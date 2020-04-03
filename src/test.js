5ba37b6f54312cce8e3825d03cb2461eb23c4f67
unknown <Administrator@SLHAYWOO2.redmond.corp.microsoft.com>
2020-04-03 11:27:54 -0700

    Add try-catch to test script

--- src/test.js
+++ src/test.js
@@ -1,16 +1,20 @@
 const sql = require('mssql/msnodesqlv8');
 
 (async () => {
-    await sql.connect({
-        server: 'localhost\\SQLEXPRESS',
-        database: 'TrainingRecords',
-        options: {
-            trustedConnection: true
-        }
-    });
+    try {
+        await sql.connect({
+            server: 'localhost\\SQLEXPRESS',
+            database: 'TrainingRecords',
+            options: {
+                trustedConnection: true
+            }
+        });
 
-    const result = await sql.query`select * from dbo.Users`;
-    console.log(result.recordset);
-
-    sql.close();
+        const result = await sql.query`select * from dbo.Users`;
+        console.log(result.recordset);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        sql.close();
+    }
 })();
